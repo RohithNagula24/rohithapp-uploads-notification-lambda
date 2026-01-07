2c966ff927eac05b7886dd400e2567387ff3d41c
Rohith2431 <nagula_rohith@epam.com>
2026-01-07 14:21:51 +0530

    Initial Lambda SAM project

---
+++ src/main/java/com/rohith/lambda/UploadsNotificationHandler.java
@@ -0,0 +1,28 @@
+package com.rohith.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+
+public class UploadsNotificationHandler
+        implements RequestHandler<SQSEvent, Void> {
+
+    private static final SnsClient snsClient = SnsClient.create();
+    private static final String TOPIC_ARN =
+            System.getenv("SNS_TOPIC_ARN");
+
+    @Override
+    public Void handleRequest(SQSEvent event, Context context) {
+
+        event.getRecords().forEach(record -> {
+            snsClient.publish(PublishRequest.builder()
+                    .topicArn(TOPIC_ARN)
+                    .message(record.getBody())
+                    .build());
+        });
+
+        return null;
+    }
+}
